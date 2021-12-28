8b6b99eeea04cf1163317d218d4448f72891a33a
YouKnowKim <h3969@naver.com>
2021-12-28 10:52:38 +0900

    enable detail post

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations("file:///C:/upload/");
+//        registry.addResourceHandler("/upload/**").addResourceLocations("file:///C:/upload/");
+        registry.addResourceHandler("/upload/**").addResourceLocations("/home/upload/");
     }
 }
