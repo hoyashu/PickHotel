209fbbc5a99bb8364c30d3d20e22c03f3f9066e1
YouKnowKim <h3969@naver.com>
2022-01-03 11:47:04 +0900

    [추가] rest api 기보본 기능

--- src/main/java/com/example/board/model/RoomVo.java
+++ src/main/java/com/example/board/model/RoomVo.java
@@ -1,17 +1,21 @@
 package com.example.board.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+import java.util.List;
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class RoomVo {
-    private int roomNo;
+    private int no;
     private String roomName;
     private String roomDeco;
     private String roomAddress;
     private String systemFileName;
     private String originalFileName;
+    private byte[] imageBytes;
+    private List<Link> links;
 }
