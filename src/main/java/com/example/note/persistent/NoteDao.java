b321b1d6389e37c5483618a29f0ea14adca0af8a
YouKnowKim <h3969@naver.com>
2022-01-17 12:47:29 +0900

    [추가] 쪽지 기능

---
+++ src/main/java/com/example/note/persistent/NoteDao.java
@@ -0,0 +1,136 @@
+package com.example.note.persistent;
+
+import com.example.note.model.NoteVo;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository("noteDao")
+public class NoteDao {
+    @Autowired
+    private SqlSession sqlSession;
+
+    // 쪽지 저장
+    public void insertNote(String noteCon){
+        this.sqlSession.insert("NoteDao.insertNote", noteCon);
+    }
+
+    // 쪽지 번호 가져오기
+    public int selectLastInsertID(){
+        return this.sqlSession.selectOne("NoteDao.selectLastInsertID");
+    }
+
+    // 쪽지 인덱스 저장
+    public void insertNoteIndex(int noteNo, int noteGetMbNo, int noteSendMbNo){
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("noteNo", noteNo);
+        map.put("noteGetMbNo", noteGetMbNo);
+        map.put("noteSendMbNo", noteSendMbNo);
+
+        this.sqlSession.insert("NoteDao.insertNoteIndex", map);
+    }
+
+    // 아이디로 멤버 번호 조회
+    public int selectMbNo(String mbId){
+        return this.sqlSession.selectOne("NoteDao.selectMbNo", mbId);
+    }
+
+    // 쪽지 상세 조회
+    public String selectDetailNote(int noteNo){
+        return this.sqlSession.selectOne("NoteDao.selectDetailNote", noteNo);
+    }
+
+    // 수신 쪽지 목록 조회
+    public List<NoteVo> selectReceiveNoteList(NoteVo params){
+        return this.sqlSession.selectList("NoteDao.selectReceiveNoteList", params);
+    }
+
+    // 수신 쪽지 카운트
+    public int selectReceiveNoteCount(NoteVo params){
+        return this.sqlSession.selectOne("NoteDao.selectReceiveNoteCount", params);
+    }
+
+
+    // 발신 쪽지 목록 조회
+    public List<NoteVo> selectSendNoteList(NoteVo params){
+        return this.sqlSession.selectList("NoteDao.selectSendNoteList", params);
+    }
+
+    // 발신 쪽지 카운트
+    public int selectSendNoteCount(NoteVo params){
+        return this.sqlSession.selectOne("NoteDao.selectSendNoteCount", params);
+    }
+
+    // 보관함 쪽지 목록 조회
+    public List<NoteVo> selectSaveNoteList(NoteVo params){
+        return this.sqlSession.selectList("NoteDao.selectSaveNoteList", params);
+    }
+
+    // 보관함 쪽지 카운트
+    public int selectSaveNoteCount(NoteVo params){
+        return this.sqlSession.selectOne("NoteDao.selectSaveNoteCount", params);
+    }
+
+    // 선택된 수신 쪽지 삭제처리
+    public void deleteReceiveNotes(int mbNo, int noteNo){
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("mbNo", mbNo);
+        map.put("noteNo", noteNo);
+
+        this.sqlSession.update("NoteDao.deleteReceiveNotes", map);
+    }
+
+    // 선택된 발신 쪽지 삭제처리
+    public void deleteSendNotes(int mbNo, int noteNo){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("mbNo", mbNo);
+        map.put("noteNo", noteNo);
+
+        this.sqlSession.update("NoteDao.deleteSendNotes", map);
+    }
+
+    // 선택된 보관함 수신 쪽지 삭제처리
+    public void deleteSaveReceiveNotes(int mbNo, int noteNo){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("mbNo", mbNo);
+        map.put("noteNo", noteNo);
+
+        this.sqlSession.update("NoteDao.deleteSaveReceiveNotes", map);
+    }
+
+    // 선택된 보관함 발신 쪽지 삭제처리
+    public void deleteSaveSendNotes(int mbNo, int noteNo) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("mbNo", mbNo);
+        map.put("noteNo", noteNo);
+
+        this.sqlSession.update("NoteDao.deleteSaveSendNotes", map);
+    }
+
+    // 선택된 받은쪽지 보관함 저장
+    public void updateSaveRetrieveNotes(int mbNo, int noteNo){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("mbNo", mbNo);
+        map.put("noteNo", noteNo);
+
+        this.sqlSession.update("NoteDao.updateSaveRetrieveNotes", map);
+    }
+
+    // 선택된 보낸쪽지 보관함 저장
+    public void updateSaveSendNotes(int mbNo, int noteNo){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("mbNo", mbNo);
+        map.put("noteNo", noteNo);
+
+        this.sqlSession.update("NoteDao.updateSaveSendNotes", map);
+    }
+
+    // 있는 회원 확인
+    public int checkMbId(String mbId){
+        return this.sqlSession.selectOne("NoteDao.checkMbId", mbId);
+    }
+}
---
+++ src/main/java/com/example/note/service/NoteService.java
@@ -0,0 +1,125 @@
+package com.example.note.service;
+
+import com.example.common.paging.PaginationInfo;
+import com.example.note.model.NoteVo;
+import com.example.note.persistent.NoteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@Service("noteService")
+public class NoteService {
+    @Autowired
+    private NoteDao noteDao;
+
+    // 쪽지 저장
+    public void registerNote(int noteSendMbNo, String noteCon, List<Integer> getMbNos){
+        this.noteDao.insertNote(noteCon);
+        int noteNo = this.noteDao.selectLastInsertID();
+
+        for(Integer noteGetMbNo : getMbNos){
+            this.noteDao.insertNoteIndex(noteNo, noteGetMbNo, noteSendMbNo);
+        }
+    }
+
+    public int retrieveMbNo(String mbId){
+        return this.noteDao.selectMbNo(mbId);
+    }
+
+    // 쪽지 상세 조회
+    public String retrieveDetailNote(int noteNo){
+        return this.noteDao.selectDetailNote(noteNo);
+    }
+
+    // 수신 쪽지 목록 조회
+    public List<NoteVo> retrieveReceiveNoteList(NoteVo params){
+        List<NoteVo> noteList = new ArrayList<NoteVo>();
+
+        int noteCount = this.noteDao.selectReceiveNoteCount(params);
+        PaginationInfo paginationInfo = new PaginationInfo(params);
+        paginationInfo.setTotalRecordCount(noteCount);
+
+        params.setPaginationInfo(paginationInfo);
+
+        if(noteCount > 0) {
+            noteList = this.noteDao.selectReceiveNoteList(params);
+        }
+
+        return noteList;
+    }
+
+
+    // 발신 쪽지 목록 조회
+    public List<NoteVo> retrieveSendNoteList(NoteVo params){
+        List<NoteVo> noteList = new ArrayList<NoteVo>();
+
+        int noteCount = this.noteDao.selectSendNoteCount(params);
+        PaginationInfo paginationInfo = new PaginationInfo(params);
+        paginationInfo.setTotalRecordCount(noteCount);
+
+        params.setPaginationInfo(paginationInfo);
+
+        if(noteCount > 0) {
+            noteList = this.noteDao.selectSendNoteList(params);
+        }
+
+        return noteList;
+    }
+
+    // 보관함 쪽지 목록 조회
+    public List<NoteVo> retrieveSaveNoteList(NoteVo params){
+        List<NoteVo> noteList = new ArrayList<NoteVo>();
+
+        int noteCount = this.noteDao.selectSendNoteCount(params);
+        PaginationInfo paginationInfo = new PaginationInfo(params);
+        paginationInfo.setTotalRecordCount(noteCount);
+
+        params.setPaginationInfo(paginationInfo);
+
+        if(noteCount > 0) {
+            noteList = this.noteDao.selectSaveNoteList(params);
+        }
+
+        return noteList;
+    }
+
+    // 선택된 수신 쪽지 삭제처리
+    public void removeReceiveNotes(int mbNo, int noteNo){
+        this.noteDao.deleteReceiveNotes(mbNo, noteNo);
+    }
+
+    // 선택된 발신 쪽지 삭제처리
+    public void removeSendNotes(int mbNo, int noteNo){
+        this.noteDao.deleteSendNotes(mbNo, noteNo);
+    }
+
+    // 선택된 보관함 수신 쪽지 삭제처리
+    public void removeSaveReceiveNotes(int mbNo, int noteNo){
+        this.noteDao.deleteSaveReceiveNotes(mbNo, noteNo);
+    }
+
+    // 선택된 보관함 발신 쪽지 삭제처리
+    public void removeSaveSendNotes(int mbNo, int noteNo) {
+        this.noteDao.deleteSaveSendNotes(mbNo, noteNo);
+    }
+
+    // 선택된 받은쪽지 보관함 저장
+    public void reviseSaveRetrieveNotes(int mbNo, int noteNo) {
+        this.noteDao.updateSaveRetrieveNotes(mbNo, noteNo);
+    }
+
+    // 선택된 보낸쪽지 보관함 저장
+    public void reviseSaveSendNotes(int mbNo, int noteNo) {
+        this.noteDao.updateSaveSendNotes(mbNo, noteNo);
+    }
+
+    // 있는 회원 확인
+    public int checkMbId(String mbId){
+        return this.noteDao.checkMbId(mbId);
+    }
+}
